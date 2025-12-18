fb09c45fe9845afed7ea4623c53c3edf10306f6e
Simon S <Simon@S.me>
2025-12-18 23:19:44 +0100

    Contract address from local Anvil

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 // export const zkSyncContractAddress = "0x4B5DF730c2e6b28E17013A1485E5d9BC41Efe021"
 // export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const contractAddress = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e"
 export const abi = [
   {
     inputs: [
